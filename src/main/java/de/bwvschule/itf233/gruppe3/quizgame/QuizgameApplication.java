144557ecc5c9690962afb83a25c4d6ed2f13389f
Tobias Köpke <koeptob27@k2ac.intern>
2026-03-17 12:12:29 +0100

    a window for the game

--- src/main/java/de/bwvschule/itf233/gruppe3/quizgame/QuizgameApplication.java
+++ src/main/java/de/bwvschule/itf233/gruppe3/quizgame/QuizgameApplication.java
@@ -1,15 +1,35 @@
 package de.bwvschule.itf233.gruppe3.quizgame;
 
 import de.bwvschule.itf233.gruppe3.quizgame.config.DBProfileSelector;
+import de.bwvschule.itf233.gruppe3.quizgame.desktop.GameWindow;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import javax.swing.*;
+import java.util.Arrays;
 
 @SpringBootApplication
 public class QuizgameApplication {
 
+    static SpringApplicationBuilder setupApplicationBuilder() {
+        return new SpringApplicationBuilder(QuizgameApplication.class)
+                .initializers(new DBProfileSelector());
+    }
+
+    static ConfigurableApplicationContext runApplication(boolean headless, String[] args) {
+        return setupApplicationBuilder().headless(headless).run(args);
+    }
+
 	public static void main(String[] args) {
-		new SpringApplicationBuilder(QuizgameApplication.class)
-				.initializers(new DBProfileSelector())
-				.run(args);
+        boolean isDesktop = Arrays.asList(args).contains("--desktop");
+        if(isDesktop) {
+            System.setProperty("java.awt.headless", "false");
+            ConfigurableApplicationContext context = runApplication(false, args);
+            SwingUtilities.invokeLater(GameWindow::launch);
+        } else {
+            runApplication(true, args);
+        }
 	}
 }
